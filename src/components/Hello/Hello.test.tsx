d66963806b64e60d35925d1627a5962f5f9612f3
Igor Thierry <igorthierry15@gmail.com>
2022-12-12 12:35:48 -0300

    feat: configurando jest e testing-library

---
+++ src/components/Hello/Hello.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react-native'
+import renderer from 'react-test-renderer'
+
+import { Hello } from '.'
+
+describe('<Hello />', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Hello />)
+    expect(tree).toBeTruthy()
+  })
+  it('renders the correct text', () => {
+    render(<Hello />)
+    const element = screen.getByText(
+      'Open up App.js to start working on your app!',
+    )
+    expect(element).toBeTruthy()
+  })
+})
